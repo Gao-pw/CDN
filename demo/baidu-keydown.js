30533fde157b72c6429f1dec5542c73c9d7af3b9
siroi <1152910080@qq.com>
2021-03-31 18:41:25 +0800

    fix js

--- demo/baidu-keydown.js
+++ demo/baidu-keydown.js
@@ -7,47 +7,40 @@ function bind() {
         if (!{ '37': 1, '38': 1, '39': 1, '40': 1 }[code]) return;
         event.preventDefault && event.preventDefault();
         //TODO: 请实现按键控制
+        let div = document.getElementsByClassName('current')[0];
+        let tbody = document.querySelector("tbody");
+        let parent = div.parentNode;
+        let index = div.cellIndex;
         switch (code) {
             case 37:
-                let div = document.getElementsByClassName('current')[0];
-                let index1 = div.cellIndex;
-                if (index1 == 0) {
-                    div.parentElement.childNodes[div.parentElement.childNodes.length - 1].classList.add('current');
+                if (index == 0) {
+                    parent.children[parent.childElementCount - 1].classList.add('current');
                 } else {
-                    div.previousElementSibling.classList.add("current");
+                    div.previousElementSibling.classList.add('current');
                 }
-                div.classList.remove('current');
                 break;
             case 38:
-                let div2 = document.getElementsByClassName('current')[0];
-                let index2 = div2.cellIndex;
-                if (div2.parentElement.previousSibling.previousSibling === null) {
-                    div2.parentNode.parentNode.childNodes[div2.parentNode.parentNode.childElementCount * 2 - 1].childNodes[index2].classList.add('current');
+                if (parent.previousElementSibling === null) {
+                    tbody.children[tbody.childElementCount - 1].children[index].classList.add("current");
                 } else {
-                    div2.parentNode.previousElementSibling.childNodes[index2].classList.add('current');
+                    parent.previousElementSibling.children[index].classList.add('current');
                 }
-                div2.classList.remove("current");
                 break;
             case 39:
-                let div3 = document.getElementsByClassName('current')[0];
-                let index3 = div3.cellIndex;
-                if (index3 == div3.parentNode.childNodes.length - 1) {
-                    div3.parentNode.childNodes[0].classList.add('current');
+                if (index == (parent.childElementCount - 1)) {
+                    parent.children[0].classList.add('current');
                 } else {
-                    div3.nextElementSibling.classList.add("current");
+                    div.nextElementSibling.classList.add('current');
                 }
-                div3.classList.remove('current');
                 break;
             case 40:
-                let div4 = document.getElementsByClassName('current')[0];
-                let index4 = div4.cellIndex;
-                if (div4.parentElement.nextSibling.nextSibling === null) {
-                    div4.parentNode.parentNode.childNodes[1].childNodes[index4].classList.add('current');
+                if (parent.nextElementSibling === null) {
+                    tbody.children[0].children[index].classList.add("current");
                 } else {
-                    div4.parentNode.nextElementSibling.childNodes[index4].classList.add('current');
+                    parent.nextElementSibling.children[index].classList.add("current");
                 }
-                div4.classList.remove("current");
                 break;
         }
+        div.classList.remove("current");
     };
 }
